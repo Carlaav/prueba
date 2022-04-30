92e34990d9d3360f49b5d3e39a538d7092c3e71b
Carlaav <92107798+Carlaav@users.noreply.github.com>
2022-04-30 10:50:49 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,1 +1,16 @@
+from multiprocessing import Pool
+import random
+import time
 
+def func(n):
+    print("starting", n)
+    duration = round(random.random(), 3)
+    time.sleep(duration)
+    print("finished", n, "time takem", duration, "seconds")
+    return n, duration
+
+urls = ["a.com", "b.com", "c.com", "d.com", "e.com", "f.com"]
+
+if __name__ == "__main__":
+    pool = Pool(processes=5)
+    data = pool.map(func, urls)
